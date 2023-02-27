43deb0af9a7d10c9020a831bb0ccd9d8525c6be8
NitroGuy10 <naviyer10@gmail.com>
2023-02-27 16:15:46 -0600

    Better background and styling

--- page.js
+++ page.js
@@ -1,6 +1,23 @@
 
-let image = document.createElement("IMG")
-image.src = "Heart2Heart.png"
-image.alt = "Heart2Heart"
-image.id = "title-image"
-document.getElementById("canvas").after(image)
+let canvas = document.getElementById("canvas")
+
+// Title image
+let image = document.createElement("IMG");
+image.src = "Heart2Heart.png";
+image.alt = "Heart2Heart";
+image.id = "title-image";
+canvas.after(image);
+
+// Background
+let background = document.createElement("DIV");
+background.id = "bg";
+canvas.before(background);
+
+// Spacing
+let space1 = document.createElement("BR");
+canvas.before(space1);
+let space2 = document.createElement("BR");
+canvas.after(space2);
+let space3 = document.createElement("BR");
+image.after(space3);
+
