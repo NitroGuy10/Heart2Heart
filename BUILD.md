606ba6e8da86178b73bfa968cb6bfd9d2d5220fc
NitroGuy10 <naviyer10@gmail.com>
2023-02-27 15:46:11 -0600

    Added logo

---
+++ BUILD.md
@@ -0,0 +1,11 @@
+
+Add to \<head\>
+
+```html
+<link rel="stylesheet" href="style.css">
+<script src="page.js" defer></script>
+```
+
+
+
+
---
+++ page.js
@@ -0,0 +1,6 @@
+
+let image = document.createElement("IMG")
+image.src = "Heart2Heart.png"
+image.alt = "Heart2Heart"
+image.id = "title-image"
+document.getElementById("canvas").after(image)
