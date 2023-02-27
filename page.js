6c7f6e792ab62a0ff2f8710552268240b5b34514
NitroGuy10 <naviyer10@gmail.com>
2023-02-27 17:18:53 -0600

    Fixed the sizing hopefully

--- page.js
+++ page.js
@@ -14,10 +14,13 @@ background.id = "bg";
 canvas.before(background);
 
 // Spacing
-let space1 = document.createElement("BR");
+let space1 = document.createElement("DIV");
+space1.className = "space-ifbig";
 canvas.before(space1);
-let space2 = document.createElement("BR");
+let space2 = document.createElement("DIV");
+space2.className = "space";
 canvas.after(space2);
-let space3 = document.createElement("BR");
+let space3 = document.createElement("DIV");
+space3.className = "space";
 image.after(space3);
 
